54fb4088584dccbaab3e78aff59bb9e40f022d18
muise001 <emiel.muis@hva.nl>
2019-03-20 08:42:00 +0100

    Added gulp

--- site/PM/tasks/revision-hash.js
+++ site/PM/tasks/revision-hash.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
 const rev = require('gulp-rev');
-const inputDir = "public/";
-const outputDir = 'cache/';
+const inputDir = "cache/";
+const revReplace = require('gulp-rev-replace');
 const manifestFilename = 'rev-manifest.json';
+
 gulp.src(["public/" + '**/*.{css,js}' ])
   .pipe(rev())
   .pipe(gulp.dest(inputDir))
   .pipe(rev.manifest(manifestFilename))
-  .pipe(gulp.dest(outputDir));
+  .pipe(gulp.dest(inputDir));
+
+  gulp.src("views/partials/" + '**/*.ejs')
+    .pipe(revReplace({manifest: gulp.src(inputDir + manifestFilename)}))
+    .pipe(gulp.dest("views/partials1"));
